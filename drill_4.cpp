e963588e8d700c2a0448df0021fa224c0013fa1b
pbalazs0223 <55666885+pbalazs0223@users.noreply.github.com>
2019-10-04 21:51:27 +0200

    Add files via upload

---
+++ drill_4.cpp
@@ -0,0 +1,45 @@
+#include "std_lib_facilities.h"
+int main()
+{
+/*1.
+int a,b;
+while (cin>>a>>b)
+    {
+        if (a == '|' || b== '|')
+        	exit(1);
+        else
+        	cout <<"a= "<< a << ", b= " << b <<;       
+    } 
+ 
+//2.,3.
+int a,b;
+while (cin>>a>>b)
+    {
+        if (a==b)
+        	cout <<"the numbers are equal";
+        else
+		if (a<b)
+        	cout <<"the smaller value is:"<<a<< " and the larger value is:"<<b;
+        else
+        	cout <<"the smaller value is:"<<b<< " and the larger value is:"<<a;         
+    } 
+
+//4.,5.
+double a,b;
+while (cin>>a>>b)
+    {
+        if (a==b)
+        	cout <<"the numbers are equal";
+        else
+		if (a<b)
+        	cout <<"the smaller value is:"<<a<< " and the larger value is:"<<b;
+        else
+        	cout <<"the smaller value is:"<<b<< " and the larger value is:"<<a;         
+    } 
+
+//6.
+
+//7.
+
+}
+	
