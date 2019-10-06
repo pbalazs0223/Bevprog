32aed74193b94118f3e4c47647862e752db0129d
pbalazs0223 <55666885+pbalazs0223@users.noreply.github.com>
2019-10-06 02:47:37 +0200

    Add files via upload

---
+++ ch3_2.cpp
@@ -0,0 +1,10 @@
+#include "std_lib_facilities.h"
+int main()
+{
+cout << "Please enter your first name and age\n";
+string first_name;
+int age;
+cin >> first_name;
+cin >> age;
+cout << "Hello, " << first_name << " (age " << age << ")\n";
+}
